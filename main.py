b1798e6cf7d18c56d51c3d4607e288fcc8ea4df9
Smertin Nikita Job <smertin-nikita@mail.ru>
2020-11-20 15:36:59 +0500

    Init Task2

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import os
 import collections as c
 
+
+def print_cook_book(cook_book):
+    for dish in cook_book:
+        print(dish)
+        for ingredient in cook_book[dish]:
+            print('     ', ingredient)
+        print()
+
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredient in cook_book[dish]:
+                name = ingredient['ingredient_name']
+                quantity = int(ingredient['quantity']) * person_count
+                measure = ingredient['measure']
+                shop_list[name] = {
+                    'quantity': quantity,
+                    'measure': measure
+                }
+    return shop_list
+
+
 cook_book = {}
 Ingredient = c.namedtuple('Ingredient', ['ingredient_name', 'quantity', 'measure'])
 
@@ -8,16 +32,17 @@ file_path = os.path.join(os.getcwd(), '2.4.files/recipes.txt')
 with open(file_path, encoding='utf-8') as f:
     for line in f:
         if line != '\n':
-            dishes = line.rstrip('\n')
-            cook_book[dishes] = []
+            dish = line.rstrip('\n')
+            cook_book[dish] = []
 
             for ingredient in range(int(next(f))):
                 ingredient_name, quantity, measure = next(f).rstrip('\n').split(' | ')
-                cook_book[dishes].append({
+                cook_book[dish].append({
                     'ingredient_name': ingredient_name,
                     'quantity': quantity,
                     'measure': measure
                 })
 
 
-print(cook_book)
+print_cook_book(cook_book)
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
